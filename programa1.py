2b412c5b302c6769a65d887e6e086b1a4f5ea4a4
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2025-10-01 09:18:04 -0600

    Create programa1.py

---
+++ programa1.py
@@ -0,0 +1,6 @@
+# Este es un script de python de prueba para el repositorio
+def saludo(nombre):
+  return "Hola "+nombre
+
+# se invoca a la funcion saludo para saludar a alguien llamado Carlos
+saludo("Carlos")
